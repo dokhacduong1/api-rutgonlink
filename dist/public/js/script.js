ac9bed16cf074f49c8a36d094e1cf4489b5b9188
dokhacduong3 <dokhacduong3@gmail.com>
2024-01-12 00:23:36 +0700

    done-vipv5

--- dist/public/js/script.js
+++ dist/public/js/script.js
@@ -1,14 +1,33 @@
-function myFunction() {
-    // Get the text field
-    var copyText = document.getElementById("myInput");
-  
-    // Select the text field
-    copyText.select();
-    copyText.setSelectionRange(0, 99999); // For mobile devices
-  
-     // Copy the text inside the text field
-    navigator.clipboard.writeText(copyText.value);
-  
-    // Alert the copied text
-    alert("Đã copy key: " + copyText.value);
-  }
+const clickLink = document.querySelector('.click-link');
+
+if (clickLink) {
+  clickLink.addEventListener('click', async function () {
+    const waitLink = document.querySelector('.wait-link');
+    waitLink.style.display = 'block';
+
+    try {
+      const response = await fetch("/api/v1/get-link", {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+      })
+      const data = await response.json();
+
+      if (data.code === 200) {
+        window.location.href = data.link;
+      } else if (data.code === 400) {
+        alert(data.message);
+        window.location.reload();
+      } else if (data.code === 500) {
+        alert(data.message)
+        window.location.reload();
+      }
+    } catch (error) {
+      window.location.reload();
+      // Xử lý lỗi, ví dụ thông báo hoặc không làm gì cả
+    } finally {
+      waitLink.style.display = 'none';
+    }
+  });
+}
